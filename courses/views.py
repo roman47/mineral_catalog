d6829c90336fdce5bc2d6942ac655dad6a33e159
roman47 <dillonginley@gmail.com>
2018-12-05 12:27:03 -0500

    Fixing the random mineral button to live in layout.html and adding requirements.txt

--- courses/views.py
+++ courses/views.py
@@ -9,26 +9,35 @@ def course_list(request):
     courses = Course.objects.all()
     email = 'question@learning_site.com'
     return render(request, 'courses/course_list.html', {'courses': courses,
-                                                       'email':email})
+                                                        'email': email})
+
+
 def course_detail(request, pk):
     course = get_object_or_404(Course, pk=pk)
     return render(request, 'courses/course_detail.html', {'course': course})
+
 
 def step_detail(request, course_pk, step_pk):
     step = get_object_or_404(Step, course_id=course_pk, pk=step_pk)
     return render(request, 'courses/step_detail.html', {'step': step})
 
+
 def index(request):
     minerals = Mineral.objects.all()
     rand = random_mineral_pk
-    return render(request, 'courses/index.html', {'minerals': minerals, 'rand': rand})
-  
+    return render(request, 'courses/index.html',
+                  {'minerals': minerals, 'rand': rand})
+
+
 def detail(request, pk):
     mineral = get_object_or_404(Mineral, pk=pk)
-    return render(request, 'courses/detail.html', {'mineral': mineral})
-  
+    rand = random_mineral_pk
+    return render(request, 'courses/detail.html',
+                  {'mineral': mineral, 'rand': rand})
+
+
 def random_mineral_pk():
-      '''Gets a random primary key from the mineral table'''
-      count = Mineral.objects.latest('pk').pk
-      random_index = random.randint(0, count - 1)
-      return random_index
+    '''Gets a random primary key from the mineral table'''
+    count = Mineral.objects.latest('pk').pk
+    random_index = random.randint(0, count - 1)
+    return random_index
